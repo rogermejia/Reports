5d4e38567edbc53680c00f7fc9cb941e4bbcdbf7
Rogelio <Roger.Alonso7412@gmail.com>
2019-12-05 09:06:40 -0600

    Initial commit

---
+++ RepportingSystem/src/main/java/com/report/system/entities/Products.java
@@ -0,0 +1,191 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.report.system.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author rogelio.mejia
+ */
+@Entity
+@Table(name = "products")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Products.findAll", query = "SELECT p FROM Products p")
+    , @NamedQuery(name = "Products.findByProductID", query = "SELECT p FROM Products p WHERE p.productID = :productID")
+    , @NamedQuery(name = "Products.findByProductName", query = "SELECT p FROM Products p WHERE p.productName = :productName")
+    , @NamedQuery(name = "Products.findBySupplierID", query = "SELECT p FROM Products p WHERE p.supplierID = :supplierID")
+    , @NamedQuery(name = "Products.findByCategoryID", query = "SELECT p FROM Products p WHERE p.categoryID = :categoryID")
+    , @NamedQuery(name = "Products.findByQuantityPerUnit", query = "SELECT p FROM Products p WHERE p.quantityPerUnit = :quantityPerUnit")
+    , @NamedQuery(name = "Products.findByUnitPrice", query = "SELECT p FROM Products p WHERE p.unitPrice = :unitPrice")
+    , @NamedQuery(name = "Products.findByUnitsInStock", query = "SELECT p FROM Products p WHERE p.unitsInStock = :unitsInStock")
+    , @NamedQuery(name = "Products.findByUnitsOnOrder", query = "SELECT p FROM Products p WHERE p.unitsOnOrder = :unitsOnOrder")
+    , @NamedQuery(name = "Products.findByReorderLevel", query = "SELECT p FROM Products p WHERE p.reorderLevel = :reorderLevel")
+    , @NamedQuery(name = "Products.findByDiscontinued", query = "SELECT p FROM Products p WHERE p.discontinued = :discontinued")})
+public class Products implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ProductID")
+    private Integer productID;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 40)
+    @Column(name = "ProductName")
+    private String productName;
+    @Column(name = "SupplierID")
+    private Integer supplierID;
+    @Column(name = "CategoryID")
+    private Integer categoryID;
+    @Size(max = 20)
+    @Column(name = "QuantityPerUnit")
+    private String quantityPerUnit;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "UnitPrice")
+    private BigDecimal unitPrice;
+    @Column(name = "UnitsInStock")
+    private Integer unitsInStock;
+    @Column(name = "UnitsOnOrder")
+    private Integer unitsOnOrder;
+    @Column(name = "ReorderLevel")
+    private Integer reorderLevel;
+    @Column(name = "Discontinued")
+    private Boolean discontinued;
+
+    public Products() {
+    }
+
+    public Products(Integer productID) {
+        this.productID = productID;
+    }
+
+    public Products(Integer productID, String productName) {
+        this.productID = productID;
+        this.productName = productName;
+    }
+
+    public Integer getProductID() {
+        return productID;
+    }
+
+    public void setProductID(Integer productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Integer getSupplierID() {
+        return supplierID;
+    }
+
+    public void setSupplierID(Integer supplierID) {
+        this.supplierID = supplierID;
+    }
+
+    public Integer getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(Integer categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getQuantityPerUnit() {
+        return quantityPerUnit;
+    }
+
+    public void setQuantityPerUnit(String quantityPerUnit) {
+        this.quantityPerUnit = quantityPerUnit;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public Integer getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(Integer unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public Integer getUnitsOnOrder() {
+        return unitsOnOrder;
+    }
+
+    public void setUnitsOnOrder(Integer unitsOnOrder) {
+        this.unitsOnOrder = unitsOnOrder;
+    }
+
+    public Integer getReorderLevel() {
+        return reorderLevel;
+    }
+
+    public void setReorderLevel(Integer reorderLevel) {
+        this.reorderLevel = reorderLevel;
+    }
+
+    public Boolean getDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(Boolean discontinued) {
+        this.discontinued = discontinued;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (productID != null ? productID.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Products)) {
+            return false;
+        }
+        Products other = (Products) object;
+        if ((this.productID == null && other.productID != null) || (this.productID != null && !this.productID.equals(other.productID))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.report.system.dao.Products[ productID=" + productID + " ]";
+    }
+    
+}
